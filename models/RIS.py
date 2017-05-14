2575ca84f8a850a6ac9648175e7022e789ab59cc
Leon Chambers <leon8556@gmail.com>
2017-05-13 21:12:36 -0400

    Working on RIS integration

--- models/RIS.py
+++ models/RIS.py
@@ -1,19 +1,33 @@
+import os
 import re
 from .base import LeafSegmentationModel
 
 class Model(LeafSegmentationModel):
-    def train(self, data):
+    def pre_train(self, data):
         # write data_ and label_ lists
         images = data.images(self.run_id)
         data_list = [
             image[0] for image in images
         ]
+        with open("data_list.txt", "w+") as f:
+            f.write("\n".join(data_list))
         label_list = [
             re.sub("_rgb.png$", "_label.png", path) for path in data_list
         ]
+        with open("label_list.txt", "w+") as f:
+            f.write("\n".join(label_list))
 
-        # Call the RIS code
-        raise NotImplementedError()
+    def train(self, data):
+        cmd = [
+            "th", "../RIS/plants_learning/experiment.lua", "-name",
+            self.run_id, "-height", 200, "-width", 200, "-data_dir", "."
+        ]
+        if self.dry:
+            cmd.extend(["-it", "100"])
+
+    def post_train(self, data):
+        os.remove("data_list.txt")
+        os.remove("label_list.txt")
 
     def test(self, data):
         raise NotImplementedError()
